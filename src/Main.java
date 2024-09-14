20491a39dc9586aa4404937c6c62c725cf51da86
Marcelo <marcelopandocerp@gmail.com>
2024-09-13 22:20:42 -0300

    agregada division entre 0

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hola mundo!");
+        System.out.printf("Bienvenido al sistema");
 
+
+        try {
+            int var = 3/0;}
+
+        catch (Exception e) {
+            System.out.println("\nno se puede dividir entre 0.");
+        }
     }
 }
